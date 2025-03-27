dc3645376c329e2569acdd8ada37a136e2ab3bc3
Gabriel Bessi <tecbessi@gmail.com>
2025-03-27 06:44:18 -0300

    Implementando animações background

--- joguinho/code/level.py
+++ joguinho/code/level.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from code.entity import Entity
+from code.entityFactory import EntityFactory
+import pygame as pg
 
 
 class Level:
@@ -9,6 +11,12 @@ class Level:
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
 
-    def run(self, ):
+    def run(self):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move()
+            pg.display.flip()
         pass
